9d3264e48a33b9bf4de6a87298c430ea6ab2318c
Paulo Goncalves <phsgjc@gmail.com>
2018-10-27 15:45:30 +1100

    Begin to add phpunit test

---
+++ api/tests/CatapultTest.php
@@ -0,0 +1,17 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+
+final class CatapultTest extends TestCase
+{
+    
+    public function test()
+    {
+        
+        $this->assertTrue(true);
+    
+    }
+
+}
+
+?>
